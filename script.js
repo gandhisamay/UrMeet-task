58ba7f85f4536cd68e6fced4f0cb1729667a6972
gandhisamay <gandhisamay200@gmail.com>
2021-08-13 20:02:54 +0530

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+const cats = document.querySelectorAll('.cat');
+const categoryList = document.querySelector('.category-list');
+
+for(let cat of cats){
+    cat.addEventListener('mouseenter', ()=>{
+        categoryList.classList.toggle('visible');
+    })
+    cat.addEventListener('mouseleave', () => {
+        categoryList.classList.toggle('visible');
+    })
+}
+
+
